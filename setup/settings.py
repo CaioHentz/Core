4ae9d2cdd66406ee892f99577683b48b58c77db4
João Neumann <161613347+neumannjoao@users.noreply.github.com>
2025-11-06 23:44:31 -0300

    .

--- setup/settings.py
+++ setup/settings.py
@@ -113,11 +113,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'setup', 'static'),
+    BASE_DIR / 'static',
+    BASE_DIR / 'setup' / 'static',
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -128,8 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # WhiteNoise para servir arquivos estáticos no Railway
-if not DEBUG:
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Configuração para servir arquivos de mídia (opcional, mas boa prática)
 MEDIA_URL = '/media/'
