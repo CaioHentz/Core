ff73f7ec6c6befb2e8fe4adc1c7e758045689215
Caio Hentz <caiohps2006@gmail.com>
2025-11-10 19:53:32 -0300

    corrected CSRF token error

--- setup/settings.py
+++ setup/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['.up.railway.app', 'localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://*.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://core-production-8197.up.railway.app']
 
 # Reconhecer HTTPS corretamente
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
