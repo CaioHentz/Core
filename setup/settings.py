7bdcad7acdbc00ca99e21d2d3b56433a60023885
Caio Hentz <caiohps2006@gmail.com>
2025-11-10 19:45:15 -0300

    corrected bad request issue

--- setup/settings.py
+++ setup/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['core-production-8197.up.railway.app']
+ALLOWED_HOSTS = ['core-production-8197.up.railway.app', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://core-production-8197.up.railway.app']
 
 # Reconhecer HTTPS corretamente
